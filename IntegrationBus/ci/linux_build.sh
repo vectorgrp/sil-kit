17f51cd85ee710ccf59cbee790c1a968bd5903ba
Nico Hübschmann <nico.huebschmann@vector.com>
2019-07-16 12:00:56 +0200

    VIB-2 Build docs only if BUILD_DOCS is set

--- IntegrationBus/ci/linux_build.sh
+++ IntegrationBus/ci/linux_build.sh
@@ -35,6 +35,7 @@ cmake .. \
     -DCMAKE_BUILD_TYPE=$target \
     -DIB_BUILD_NUMBER=$buildNumber \
     -DIB_INSTALL_PDB_FILES=ON \
+    -DBUILD_DOCS=ON \
     -DCMAKE_INSTALL_PREFIX=../install \
     -DCMAKE_PREFIX_PATH=./install \
     -DCMAKE_INSTALL_SYMLINKS=OFF \
