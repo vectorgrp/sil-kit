08902c52f1d8e4f470b170453eb0aeba7de0c169
Marius Börschig <Marius.Boerschig@vector.com>
2019-10-30 15:53:08 +0100

    AFTMAGT-195: add IB_INSTALL_SOURCE option
    - the new option IB_INSTALL_SOURCE deploys the source tree for
      packaging
    
      - the IntegrationBus/  directory is bundled as Source/
    
      - the packaged Source/Demos and Source/ folders should be usable
        directly from Visual Studio (right-click open-in)
    
    - this also fixes a number of build quirks:
      - cmake/msbuild was confused about toolset, caused by the
        package_project macros
      - always append 'd' suffix in vstudio build (for merging cpack zips)
    
    - a workaround includes ThirdParty/fmt-header-only for the
      exported binary IntegrationBus library's cmake configuration.
      WIP will be fixed later, because the binary distribution relies on the
      deployed source tree

--- ThirdParty/fmt.cmake
+++ ThirdParty/fmt.cmake
@@ -1,13 +1,14 @@
-# XXX fmt is in our public ILogger.hpp, and also in the interface link list of IbInterface
-# therefore it *must* be install-exported, otherwise the install-export of Integrationbus fails
-set(FMT_VERSION "5.3.0")
-set(FMT_INSTALL ON CACHE BOOL "Make sure the fmt-header-only interface target is exported" FORCE)
-message(STATUS "Add subdirectory ThirdParty/fmt-${FMT_VERSION}")
-add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/fmt-${FMT_VERSION}
-    ${CMAKE_BINARY_DIR}/ThirdParty/fmt-${FMT_VERSION}
-    EXCLUDE_FROM_ALL
-)
-#TODO remove fmt from our publicly visible interfaces/dependencies
-install(TARGETS fmt-header-only
-    PUBLIC_HEADER DESTINATION ${INSTALL_INCLUDE_DIR}
-)
+function(include_fmt)
+    # function() adds an additional scope
+    # XXX fmt is in our public ILogger.hpp, and also in the interface link list of IbInterface
+    # therefore it *must* be install-exported, otherwise the install-export of Integrationbus fails
+    set(FMT_VERSION "5.3.0")
+    set(FMT_INSTALL ON CACHE BOOL "Make sure the fmt-header-only interface target is exported" FORCE)
+    message(STATUS "Add subdirectory ${CMAKE_CURRENT_LIST_DIR}/fmt-${FMT_VERSION}")
+    add_subdirectory(${CMAKE_CURRENT_LIST_DIR}/fmt-${FMT_VERSION}
+        ${CMAKE_BINARY_DIR}/ThirdParty/fmt-${FMT_VERSION}
+        EXCLUDE_FROM_ALL
+    )
+endfunction()
+
+include_fmt()
