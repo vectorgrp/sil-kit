e3ab938b9887111bd60cf6ff4f4809148e715eb8
Marius Börschig <Marius.Boerschig@vector.com>
2020-10-29 08:45:40 +0100

    -- cmake: Add options IB_ENABLE_UBSAN and IB_ENABLE_ASAN
    When setting these options the asan/ubsan sanitizers are enabled.
    These are compiler specific and are not always supported (currently used
    with GCC).
    
    When using ubsan: check the output of ctest for 'runtime error:'
    markers.
    
    Also add an ib_enable_warnings() cmake macro to increase compile warning
    levels on specific targets (currently only on GCC/Clang).

---
+++ IntegrationBus/cmake/IntegrationBusBuildSettings.cmake
@@ -0,0 +1,57 @@
+function(ib_enable_asan isOn)
+    if(NOT isOn)
+        return()
+    endif()
+    message(STATUS "VIB -- Enabling Address Sanitizer")
+    if(MSVC)
+        add_compile_options(/fsanitize=address)
+    else()
+        #clang, gcc
+        add_compile_options(-fsanitize=address -fno-omit-frame-pointer )
+        #ensures that -lasan is linked in as first library
+        add_link_options(-fsanitize=address)
+    endif()
+endfunction()
+
+function(ib_enable_ubsan isOn)
+    if(NOT isOn)
+        return()
+    endif()
+    if(MSVC)
+        message(STATUS "VIB -- UBSAN not supported on MSVC")
+    else()
+        message(STATUS "VIB -- Enabling Undefined Behavior Sanitizer")
+        #clang, gcc
+        add_compile_options(-fsanitize=undefined) 
+        #ensures that -lasan is linked in as first library
+        add_link_options(-fsanitize=undefined)
+    endif()
+endfunction()
+
+function(ib_enable_warnings target)
+    if(MSVC)
+        # TODO
+        #Note this only works on CMake 3.15, otherwise it will produce command
+        # line warnings because /W3 is overriden with /W4
+        #set(_flags /W4)
+    else()
+        set(_flags
+            -pedantic
+            -Wall
+            -Wextra
+            -Wcast-align
+            -Wformat=2
+            -Wmissing-declarations
+            -Wshadow 
+            -Wsign-conversion
+            -Wsign-promo 
+            -Wstrict-overflow=5
+            -Wswitch-default
+            -Wundef
+            -Wno-unused
+            -Wabi
+            -Wpacked
+            )
+        target_compile_options(${target} PRIVATE ${_flags})
+    endif()
+endfunction()
