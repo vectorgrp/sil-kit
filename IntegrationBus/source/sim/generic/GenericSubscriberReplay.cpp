e18e9dc8fca3ca72285a602e1c6acb2ac8c5e270
Marius Börschig <Marius.Boerschig@vector.com>
2021-01-18 17:40:38 +0100

    AFTMAGT287 Add GenericSubscriber replay controller.

---
+++ IntegrationBus/source/sim/generic/GenericSubscriberReplay.cpp
@@ -0,0 +1,92 @@
+// Copyright (c) Vector Informatik GmbH. All rights reserved.
+
+#include "GenericSubscriberReplay.hpp"
+
+namespace ib {
+namespace sim {
+namespace generic {
+
+GenericSubscriberReplay::GenericSubscriberReplay(mw::IComAdapter* comAdapter,
+        cfg::GenericPort config, mw::sync::ITimeProvider* timeProvider)
+    : _subscriber{comAdapter, config, timeProvider}
+    , _replayConfig{config.replay}
+{
+
+}
+
+
+void GenericSubscriberReplay::SetReceiveMessageHandler(CallbackT callback)
+{
+    _subscriber.SetReceiveMessageHandler(std::move(callback));
+}
+
+void GenericSubscriberReplay::ReceiveIbMessage(mw::EndpointAddress from, const GenericMessage& msg)
+{
+    if (tracing::IsReplayEnabledFor(_replayConfig, cfg::Replay::Direction::Receive))
+    {
+        return;
+    }
+    return _subscriber.ReceiveIbMessage(from, msg);
+
+}
+
+auto GenericSubscriberReplay::Config() const -> const cfg::GenericPort&
+{
+    return _subscriber.Config();
+}
+
+
+void GenericSubscriberReplay::SetEndpointAddress(const ib::mw::EndpointAddress& endpointAddress)
+{
+    _subscriber.SetEndpointAddress(endpointAddress);
+}
+
+auto GenericSubscriberReplay::EndpointAddress() const -> const ib::mw::EndpointAddress&
+{
+    return _subscriber.EndpointAddress();
+}
+
+// ib::mw::sync::ITimeConsumer
+void GenericSubscriberReplay::SetTimeProvider(ib::mw::sync::ITimeProvider* timeProvider)
+{
+    _subscriber.SetTimeProvider(timeProvider);
+}
+
+// ITraceMessageSource
+void GenericSubscriberReplay::AddSink(extensions::ITraceMessageSink* sink)
+{
+    _subscriber.AddSink(sink);
+}
+
+// IReplayDataProvider
+
+void GenericSubscriberReplay::ReplayMessage(const extensions::IReplayMessage* replayMessage)
+{
+    using namespace ib::tracing;
+    switch (replayMessage->GetDirection())
+    {
+    case extensions::Direction::Receive:
+        if (IsReplayEnabledFor(_replayConfig, cfg::Replay::Direction::Send))
+        {
+            ReplayReceive(replayMessage);
+        }
+        break;
+    default:
+        throw std::runtime_error("GenericSubscriberReplay: replay message has undefined Direction");
+        break;
+    }
+
+}
+
+
+void GenericSubscriberReplay::ReplayReceive(const extensions::IReplayMessage* replayMessage)
+{
+    // need to copy the message here.
+    // will throw if invalid message type.
+    sim::generic::GenericMessage msg = dynamic_cast<const sim::generic::GenericMessage&>(*replayMessage);
+    _subscriber.ReceiveIbMessage(replayMessage->EndpointAddress(), msg);
+}
+
+} // namespace generic
+} // namespace sim
+} // namespace ib
---
+++ IntegrationBus/source/sim/generic/GenericSubscriberReplay.hpp
@@ -0,0 +1,56 @@
+// Copyright (c) Vector Informatik GmbH. All rights reserved.
+
+#pragma once
+
+#include "ib/mw/fwd_decl.hpp"
+#include "GenericSubscriber.hpp"
+#include "IReplayDataController.hpp"
+
+
+namespace ib {
+namespace sim {
+namespace generic {
+
+class GenericSubscriberReplay
+    : public IGenericSubscriber
+    , public IIbToGenericSubscriber
+    , public mw::sync::ITimeConsumer
+    , public extensions::ITraceMessageSource
+    , public tracing::IReplayDataController
+{
+public:
+    // ----------------------------------------
+    GenericSubscriberReplay(mw::IComAdapter* comAdapter, cfg::GenericPort config,
+        mw::sync::ITimeProvider* timeProvider);
+
+public:
+    // IGenericSubscriber
+    void SetReceiveMessageHandler(CallbackT callback)  override;
+
+    auto Config() const -> const cfg::GenericPort& override;
+
+    void ReceiveIbMessage(mw::EndpointAddress from, const GenericMessage& msg) override;
+    void SetEndpointAddress(const mw::EndpointAddress& endpointAddress) override;
+    auto EndpointAddress() const -> const mw::EndpointAddress& override;
+
+    //ib::mw::sync::ITimeConsumer
+    void SetTimeProvider(mw::sync::ITimeProvider* provider) override;
+
+    // ITraceMessageSource
+    void AddSink(extensions::ITraceMessageSink* sink) override;
+
+    // IReplayDataProvider
+
+    void ReplayMessage(const extensions::IReplayMessage* replayMessage) override;
+private:
+    //Private methods
+    void ReplayReceive(const extensions::IReplayMessage* replayMessage);
+private:
+    //private Members
+    cfg::Replay _replayConfig;
+    GenericSubscriber _subscriber;
+};
+
+} // namespace generic
+} // namespace sim
+} // namespace ib
