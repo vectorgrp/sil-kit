6591596b8ac9f15796fbe253be95f3a7a02a10a4
Dominik Herr <Dominik.Herr@vector.com>
2022-07-06 15:20:59 +0200

    VIB-813: Add TimeAnnotation demo

---
+++ Demos/TimeAnnotation/TimeAnnotationDemo.cpp
@@ -0,0 +1,269 @@
+// Copyright (c) Vector Informatik GmbH. All rights reserved.
+
+#include <algorithm>
+#include <cstring>
+#include <future>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <sstream>
+#include <thread>
+#include <vector>
+
+#include "silkit/SilKit.hpp"
+#include "silkit/services/logging/ILogger.hpp"
+#include "silkit/services/orchestration/all.hpp"
+#include "silkit/services/orchestration/string_utils.hpp"
+#include "silkit/services/can/all.hpp"
+#include "silkit/services/can/string_utils.hpp"
+
+using namespace SilKit::Services;
+using namespace SilKit::Services::Can;
+
+using namespace std::chrono_literals;
+
+namespace std {
+namespace chrono {
+std::ostream& operator<<(std::ostream& out, nanoseconds timestamp)
+{
+    auto seconds = std::chrono::duration_cast<std::chrono::duration<double, std::ratio<1, 1>>>(timestamp);
+    out << seconds.count() << "s";
+    return out;
+}
+} // namespace chrono
+} // namespace std
+
+void FrameTransmitHandler(const CanFrameTransmitEvent& ack, Logging::ILogger* logger)
+{
+    std::stringstream buffer;
+    buffer << ">> " << ack.status
+           << " for CAN Message with transmitId=" << ack.transmitId
+           << " timestamp=" << ack.timestamp;
+    logger->Info(buffer.str());
+}
+
+void FrameHandler(const CanFrameEvent& frameEvent, Logging::ILogger* logger)
+{
+    std::string payload(frameEvent.frame.dataField.begin(), frameEvent.frame.dataField.end());
+    std::stringstream buffer;
+    buffer << ">> CAN Message: canId=" << frameEvent.frame.canId
+           << " timestamp=" << frameEvent.timestamp
+           << " \"" << payload << "\"";
+    logger->Info(buffer.str());
+}
+
+void SendFrame(ICanController* controller, Logging::ILogger* logger)
+{
+    CanFrame canFrame {};
+    canFrame.canId = 3;
+    canFrame.flags.ide = 0; // Identifier Extension
+    canFrame.flags.rtr = 0; // Remote Transmission Request
+    canFrame.flags.fdf = 1; // FD Format Indicator
+    canFrame.flags.brs = 1; // Bit Rate Switch  (for FD Format only)
+    canFrame.flags.esi = 0; // Error State indicator (for FD Format only)
+
+    static int msgId = 0;
+    std::stringstream payloadBuilder;
+    payloadBuilder << "CAN " << (msgId++)%100;
+    auto payloadStr = payloadBuilder.str();
+
+    canFrame.dataField.assign(payloadStr.begin(), payloadStr.end());
+    canFrame.dlc = canFrame.dataField.size();
+
+    auto transmitId = controller->SendFrame(std::move(canFrame));
+    std::stringstream buffer;
+    buffer << "<< CAN Message sent with transmitId=" << transmitId;
+    logger->Info(buffer.str());
+}
+
+/**************************************************************************************************
+ * Main Function
+ **************************************************************************************************/
+
+int main(int argc, char** argv)
+{
+    if (argc < 3)
+    {
+        std::cerr << "Missing arguments! Start demo with: " << argv[0]
+                  << " <ParticipantConfiguration.yaml|json> <ParticipantName> [registryUri] [--async]" << std::endl
+                  << "Use \"CanWriter\" or \"CanReader\" as <ParticipantName>." << std::endl;
+        return -1;
+    }
+
+    if (argc > 5)
+    {
+        std::cerr << "Too many arguments! Start demo with: " << argv[0]
+                  << " <ParticipantConfiguration.yaml|json> <ParticipantName> [registryUri] [--async]" << std::endl
+                  << "Use \"CanWriter\" or \"CanReader\" as <ParticipantName>." << std::endl;
+        return -1;
+    }
+    
+    try
+    {
+        std::string participantConfigurationFilename(argv[1]);
+        std::string participantName(argv[2]);
+
+        std::string registryUri = "silkit://localhost:8500";
+
+        bool runSync = true;
+        bool runCoordinated = true;
+
+        std::vector<std::string> args;
+        std::copy((argv + 3), (argv + argc), std::back_inserter(args));
+
+        for (auto arg : args)
+        {
+            if (arg == "--async")
+            {
+                runSync = false;
+            }
+            else if (arg == "--uncoordinated")
+            {
+                runCoordinated = false;
+            }
+            else
+            {
+                registryUri = arg;
+            }
+        }
+
+        auto participantConfiguration = SilKit::Config::ParticipantConfigurationFromFile(participantConfigurationFilename);
+        auto sleepTimePerTick = 1000ms;
+
+        std::cout << "Creating participant '" << participantName << "' with registry " << registryUri << std::endl;
+
+        auto participant = SilKit::CreateParticipant(participantConfiguration, participantName, registryUri);
+
+        auto* logger = participant->GetLogger();
+        auto* canController = participant->CreateCanController("CAN1");
+
+        canController->AddFrameTransmitHandler(
+            [logger](ICanController* /*ctrl*/, const CanFrameTransmitEvent& ack) {
+                FrameTransmitHandler(ack, logger);
+            });
+        canController->AddFrameHandler(
+            [logger](ICanController* /*ctrl*/, const CanFrameEvent& frameEvent) {
+                FrameHandler(frameEvent, logger);
+            });
+
+        // always use life cycle
+        auto* lifecycleService = participant->GetLifecycleService();
+
+        // Set a CommunicationReady Handler
+        lifecycleService->SetCommunicationReadyHandler([canController, &participantName]() {
+            std::cout << "Communication ready for " << participantName << std::endl;
+            canController->SetBaudRate(10'000, 1'000'000);
+            canController->Start();
+        });
+
+        // Set a Stop Handler
+        lifecycleService->SetStopHandler([]() {
+            std::cout << "Stopping..." << std::endl;
+        });
+
+        // Set a Shutdown Handler
+        lifecycleService->SetShutdownHandler([]() {
+            std::cout << "Shutting down..." << std::endl;
+        });
+
+        if (runSync)
+        {
+            auto* timeSyncService = lifecycleService->GetTimeSyncService();
+            timeSyncService->SetPeriod(5ms);
+
+            if (participantName == "Sender")
+            {
+                timeSyncService->SetSimulationTask(
+                    [canController, logger, sleepTimePerTick](std::chrono::nanoseconds now,
+                                                              std::chrono::nanoseconds duration) {
+                        std::cout << "now=" << now << ", duration=" << duration << std::endl;
+                        SendFrame(canController, logger);
+                        std::this_thread::sleep_for(sleepTimePerTick);
+                    });
+            }
+            else
+            {
+                timeSyncService->SetSimulationTask(
+                    [sleepTimePerTick](std::chrono::nanoseconds now, std::chrono::nanoseconds duration) {
+                        std::cout << "now=" << now << ", duration=" << duration << std::endl;
+                        std::this_thread::sleep_for(sleepTimePerTick);
+                    });
+            }
+
+            auto finalStateFuture = lifecycleService->StartLifecycleWithSyncTime(timeSyncService, {runCoordinated, runCoordinated});
+            auto finalState = finalStateFuture.get();
+
+            std::cout << "Simulation stopped. Final State: " << finalState << std::endl;
+            std::cout << "Press enter to stop the process..." << std::endl;
+            std::cin.ignore();
+        }
+        else
+        {
+            bool isStopped = false;
+            std::promise<void> startSending;
+            std::thread workerThread;
+
+            // Set a Starting Handler (only triggers for asynchronous participants)
+            lifecycleService->SetStartingHandler([&]() {
+                std::cout << "Starting..." << std::endl;
+                startSending.set_value();
+            });
+
+
+            if (participantName == "Sender")
+            {
+                workerThread = std::thread{[&]() {
+                    auto startSendingFuture = startSending.get_future();
+                    startSendingFuture.get();
+
+                    while (!isStopped)
+                    {
+                        SendFrame(canController, logger);
+                        std::this_thread::sleep_for(sleepTimePerTick);
+                    }
+                    std::cout << "Sending stop signal..." << std::endl;
+                    lifecycleService->Stop("Manual stop.");
+                }};
+            }
+            else
+            {
+                workerThread = std::thread{[&]() {
+                    auto startSendingFuture = startSending.get_future();
+                    startSendingFuture.get();
+
+                    while (!isStopped)
+                    {
+                        std::this_thread::sleep_for(sleepTimePerTick);
+                    }
+                    std::cout << "Sending stop signal..." << std::endl;
+                    lifecycleService->Stop("Manual stop.");
+                }};
+            }
+
+            auto finalStateFuture =
+                lifecycleService->StartLifecycleNoSyncTime({runCoordinated, runCoordinated});
+            std::cout << "Press enter to stop the process..." << std::endl;
+            std::cin.ignore();
+            isStopped = true;
+            auto finalState = finalStateFuture.get();
+            std::cout << "Simulation stopped. Final State: " << finalState << std::endl;
+            workerThread.join();
+        }
+    }
+    catch (const SilKit::ConfigurationError& error)
+    {
+        std::cerr << "Invalid configuration: " << error.what() << std::endl;
+        std::cout << "Press enter to stop the process..." << std::endl;
+        std::cin.ignore();
+        return -2;
+    }
+    catch (const std::exception& error)
+    {
+        std::cerr << "Something went wrong: " << error.what() << std::endl;
+        std::cout << "Press enter to stop the process..." << std::endl;
+        std::cin.ignore();
+        return -3;
+    }
+
+    return 0;
+}
