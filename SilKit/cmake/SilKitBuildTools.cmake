b0751ab592db31aaea50d9b45d591f8cb0444d1e
Börschig, Marius <Marius.Boerschig@vector.com>
2022-08-12 15:50:09 +0200

    -- cmake: add SILKIT_PACKAGE_SYMBOLS option (#417)
    * -- cmake: add SILKIT_PACKAGE_SYMBOLS option
    
    Allows to disable the PDB packaging post-build step.
    In the future, we might want to generate separate ELF debug symbols,
    e.g. using objcopy and `--add-gnu-debuglink`.
    
    * -- build: split ELF debug symbols in seperate files
    
    All supported platforms now have a *-SYMBOLS.zip which contain the
    ELF debug symbols and windows PDB files, respectively.
    libSilKitd is now at 16MB instead of 82MB.

---
+++ SilKit/cmake/SilKitBuildTools.cmake
@@ -0,0 +1,41 @@
+include(CMakeFindBinUtils)
+
+macro(silkit_split_debugsymbols targetName)
+    set(_targetFile "$<TARGET_FILE:${targetName}>")
+    set(_debugFile "${SILKIT_SYMBOLS_DIR}/${targetName}${CMAKE_DEBUG_POSTFIX}.debug")
+    add_custom_command(
+        TARGET ${targetName}
+        POST_BUILD
+        COMMAND ${CMAKE_OBJCOPY} --only-keep-debug "${_targetFile}" "${_debugFile}"
+        COMMAND ${CMAKE_STRIP} "${_targetFile}"
+        COMMAND ${CMAKE_OBJCOPY} --add-gnu-debuglink="${_debugFile}" "${_targetFile}"
+        COMMENT "SIL Kit: SILKIT_PACKAGE_SYMBOLS: splitting ELF debug symbols"
+    )
+endmacro()
+
+
+macro(silkit_package_debugsymbols targetName)
+    if(MSVC)
+        add_custom_command(
+            TARGET "${targetName}"
+            POST_BUILD
+            WORKING_DIRECTORY "${SILKIT_SYMBOLS_DIR_BASE}"
+            COMMAND "${CMAKE_COMMAND}" -E tar cf "${SILKIT_SYMBOLS_DIR_NAME}.zip" --format=zip -- "${SILKIT_SYMBOLS_DIR_NAME}"
+            COMMENT "SIL Kit: SILKIT_PACKAGE_SYMBOLS: creating PDB zip"
+        )
+    endif()
+    if(UNIX AND CMAKE_BUILD_TYPE MATCHES "Debug")
+
+        silkit_split_debugsymbols("${targetName}")
+
+        file(MAKE_DIRECTORY "${SILKIT_SYMBOLS_DIR}")
+
+        add_custom_command(
+            TARGET "${targetName}"
+            POST_BUILD
+            WORKING_DIRECTORY "${SILKIT_SYMBOLS_DIR_BASE}"
+            COMMAND "${CMAKE_COMMAND}" -E tar cf "${SILKIT_SYMBOLS_DIR_NAME}.zip" --format=zip -- "${SILKIT_SYMBOLS_DIR_NAME}"
+            COMMENT "SIL Kit: SILKIT_PACKAGE_SYMBOLS: creating ELF debug symbols zip"
+        )
+    endif()
+endmacro()
