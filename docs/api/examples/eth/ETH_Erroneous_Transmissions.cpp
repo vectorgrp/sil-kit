363ade244f8bbaca4f64a342cea9d3a1bca7be0a
Daniel Edwards <daniel.edwards@vector.com>
2022-11-08 15:02:32 +0100

    SILKIT-1000: Switch the Erroneous Transmission example in the documentation to overly long payloads instead of too short payloads

--- docs/api/examples/eth/ETH_Erroneous_Transmissions.cpp
+++ docs/api/examples/eth/ETH_Erroneous_Transmissions.cpp
@@ -75,18 +75,18 @@ for (auto i = 0; i < 50; i++)
 
 // ------------------------------------------------------------
 // Erroneous Transmission: EthernetTransmitStatus::InvalidFrameFormat
-const std::string shortMessage{"A payload with less than 46 bytes is invalid."};
-const std::vector<uint8_t> shortPayload{shortMessage.begin(), shortMessage.end()};
+const std::string longMessage(4000, 'a'); // much longer than the maximum allowed Ethernet frame size of 1534 bytes
+const std::vector<uint8_t> longPayload{longMessage.begin(), longMessage.end()};
 
 EthernetFrame invalidFrame;
 std::copy(destinationAddress.begin(), destinationAddress.end(), std::back_inserter(invalidFrame));
 std::copy(sourceAddress.begin(), sourceAddress.end(), std::back_inserter(invalidFrame));
 std::copy(etherType.begin(), etherType.end(), std::back_inserter(invalidFrame));
-std::copy(shortPayload.begin(), shortPayload.end(), std::back_inserter(invalidFrame));
+std::copy(longPayload.begin(), longPayload.end(), std::back_inserter(invalidFrame));
 
 ethernetSender->SendFrame(invalidEthernetFrame);
 
 // The MessageAckHandler callback will be triggered and call the registered handler:
 sender_FrameTransmitHandler(ethernetSender, frameTransmitEvent);
 // with frameTransmitEvent.status == EthernetTransmitStatus::InvalidFrameFormat,
-// as the Ethernet frame size is too small.
+// as the Ethernet frame size is too long.
