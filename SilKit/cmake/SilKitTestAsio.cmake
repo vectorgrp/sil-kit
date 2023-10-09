8dde8292af32a508e316d62b4e14d92be32c2413
Jan Kraemer <jan.kraemer@vector.com>
2023-10-09 14:43:03 +0200

    SILKIT-1376 cmake: Add test for boost-asio
    We now test whether boost-asio is found when using third party libraries
    of the host system
    
    Signed-off-by: Jan Kraemer <jan.kraemer@vector.com>

---
+++ SilKit/cmake/SilKitTestAsio.cmake
@@ -0,0 +1,21 @@
+include(CheckCXXSourceCompiles)
+macro(silkit_check_for_asio)
+    check_cxx_source_compiles("
+#include <asio.hpp>
+
+int main()
+{
+  asio::io_context ctx;
+  ctx.run();
+  return 0;
+}
+" HAVE_ASIO_LIB)
+
+
+if(NOT HAVE_ASIO_LIB)
+    message(FATAL_ERROR "No libasio headers found")
+else()
+    message(STATUS "Libasio headers found!")
+endif()
+
+endmacro()
