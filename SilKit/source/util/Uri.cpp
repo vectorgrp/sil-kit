b8009fa57f1d77afe2358ba833aa1240ab3fc06e
Marius Börschig <Marius.Boerschig@vector.com>
2025-04-14 18:30:56 +0200

    fixup! fixup! update spdlog to 1.15.2, update fmt to 11.1.4

--- SilKit/source/util/Uri.cpp
+++ SilKit/source/util/Uri.cpp
@@ -21,6 +21,8 @@ WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE. */
 
 #include "Uri.hpp"
 
+#include <cctype>
+
 #include "silkit/participant/exception.hpp"
 
 #include "fmt/format.h"
