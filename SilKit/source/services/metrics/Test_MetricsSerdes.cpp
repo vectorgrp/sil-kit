6cb8b8c20105c80cbfd22538c721599fb11df00f
Marius Börschig <Marius.Boerschig@vector.com>
2025-07-01 15:38:08 +0200

    fixup! fixup! Revert "fixup! fixup! some testing cleanups. testing metrics not feasible, yet"

--- SilKit/source/services/metrics/Test_MetricsSerdes.cpp
+++ SilKit/source/services/metrics/Test_MetricsSerdes.cpp
@@ -21,9 +21,9 @@ using testing::ContainerEq;
 TEST(Test_MetricsSerdes, serialize_deserialize_roundtrip)
 {
     MetricsUpdate original;
-    original.metrics.emplace_back(MetricData{1, "1", MetricKind::COUNTER, "1"});
-    original.metrics.emplace_back(MetricData{2, "2", MetricKind::STATISTIC, "[1,2,3,4]"});
-    original.metrics.emplace_back(MetricData{3, "3", MetricKind::STRING_LIST, R"(["1","2","3","4"])"});
+    original.metrics.emplace_back(MetricData{1, "1", MetricKind::COUNTER, "1", {}});
+    original.metrics.emplace_back(MetricData{2, "2", MetricKind::STATISTIC, "[1,2,3,4]", {}});
+    original.metrics.emplace_back(MetricData{3, "3", MetricKind::STRING_LIST, R"(["1","2","3","4"])", {}});
 
     SilKit::Core::MessageBuffer buffer;
     VSilKit::Serialize(buffer, original);
