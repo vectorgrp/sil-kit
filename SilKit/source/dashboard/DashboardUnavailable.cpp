5c01f240f8c841a8876ee21b4f0c91e1eca35884
Marius Börschig <Marius.Boerschig@vector.com>
2025-07-21 17:45:59 +0200

    fixup! no camelcase for folders

--- SilKit/source/dashboard/DashboardUnavailable.cpp
+++ SilKit/source/dashboard/DashboardUnavailable.cpp
@@ -2,37 +2,8 @@
 //
 // SPDX-License-Identifier: MIT
 
-#include "CreateDashboard.hpp"
 #include "CreateDashboardInstance.hpp"
-
-
-namespace SilKit {
-namespace Dashboard {
-
-auto CreateDashboard(std::shared_ptr<SilKit::Config::IParticipantConfiguration> participantConfig,
-                     const std::string& registryUri,
-                     const std::string& dashboardUri) -> std::unique_ptr<SilKit::Dashboard::IDashboard>
-{
-    SILKIT_UNUSED_ARG(participantConfig);
-    SILKIT_UNUSED_ARG(registryUri);
-    SILKIT_UNUSED_ARG(dashboardUri);
-    throw SilKit::SilKitError("SIL Kit Dashboard support is disabled");
-}
-
-auto RunDashboardTest(std::shared_ptr<SilKit::Config::IParticipantConfiguration> participantConfig,
-                      const std::string& registryUri, const std::string& dashboardUri,
-                      std::function<void()> testCode) -> TestResult
-{
-    SILKIT_UNUSED_ARG(participantConfig);
-    SILKIT_UNUSED_ARG(registryUri);
-    SILKIT_UNUSED_ARG(dashboardUri);
-    SILKIT_UNUSED_ARG(testCode);
-    throw SilKit::SilKitError("SIL Kit Dashboard support is disabled");
-}
-
-} // namespace Dashboard
-} // namespace SilKit
-
+#include "silkit/participant/exception.hpp"
 
 namespace VSilKit {
 
