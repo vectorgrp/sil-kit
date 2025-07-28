849808cddedabd1de14667bcf0dcc1257bc094c5
Marius Börschig <Marius.Boerschig@vector.com>
2025-07-28 18:17:51 +0200

    drop the double copyright

--- SilKit/cmake/toolchain-mingw-w64.cmake
+++ SilKit/cmake/toolchain-mingw-w64.cmake
@@ -1,4 +1,3 @@
-# Copyright (c) 2022 Vector Informatik GmbH
 # 
 # Permission is hereby granted, free of charge, to any person obtaining
 # a copy of this software and associated documentation files (the
