93b8d97b8b31219cfa9b9204f2b00e7c901efc92
Marius Börschig <marius.boerschig@vector.com>
2025-07-30 20:32:57 +0200

    cmake: only strip on non-msvc compilers (#246)
    strip the binaries when packaging is active

--- SilKit/cmake/SilKitBuildTools.cmake
+++ SilKit/cmake/SilKitBuildTools.cmake
@@ -5,38 +5,47 @@
 include(CMakeFindBinUtils)
 
 macro(silkit_split_debugsymbols targetName)
-    set(_targetFile "$<TARGET_FILE:${targetName}>")
-    set(_debugFile "${SILKIT_SYMBOLS_DIR}/${targetName}${CMAKE_DEBUG_POSTFIX}.debug")
-    message(DEBUG "Split debugsymbols from ${_targetName} into ${_debugFile}")
-    add_custom_command(
-        TARGET ${targetName}
-        POST_BUILD
-        COMMAND ${CMAKE_OBJCOPY} --only-keep-debug "${_targetFile}" "${_debugFile}"
-        COMMAND ${CMAKE_STRIP} "${_targetFile}"
-        COMMAND ${CMAKE_OBJCOPY} --add-gnu-debuglink="${_debugFile}" "${_targetFile}"
-        COMMENT "SIL Kit: SILKIT_PACKAGE_SYMBOLS: splitting ELF debug symbols"
-    )
-endmacro()
-macro(silkit_strip_distrib targetName)
-    if(SILKIT_PACKAGE_SYMBOLS)
+    if(NOT MSVC 
+       AND (CMAKE_STRIP AND EXISTS ${CMAKE_STRIP})
+       AND (CMAKE_OBJCOPY AND EXISTS ${CMAKE_OBJCOPY})
+      )
+
         set(_targetFile "$<TARGET_FILE:${targetName}>")
+        set(_debugFile "${SILKIT_SYMBOLS_DIR}/${targetName}${CMAKE_DEBUG_POSTFIX}.debug")
+        message(DEBUG "Split debugsymbols from ${_targetName} into ${_debugFile}")
+        file(MAKE_DIRECTORY "${SILKIT_SYMBOLS_DIR}")
         add_custom_command(
             TARGET ${targetName}
             POST_BUILD
+            COMMAND ${CMAKE_OBJCOPY} --only-keep-debug "${_targetFile}" "${_debugFile}"
             COMMAND ${CMAKE_STRIP} "${_targetFile}"
-            COMMENT "SIL Kit: stripping ${_targetName}"
+            COMMAND ${CMAKE_OBJCOPY} --add-gnu-debuglink="${_debugFile}" "${_targetFile}"
+            COMMENT "SIL Kit: SILKIT_PACKAGE_SYMBOLS: splitting ELF debug symbols on ${targetName}"
         )
+    endif()
+endmacro()
+macro(silkit_strip_distrib targetName)
+    if(NOT MSVC)
+        if(SILKIT_PACKAGE_SYMBOLS)
+            set(_targetFile "$<TARGET_FILE:${targetName}>")
+            add_custom_command(
+                TARGET ${targetName}
+                POST_BUILD
+                COMMAND ${CMAKE_STRIP} "${_targetFile}"
+                COMMENT "SIL Kit: stripping ${_targetName}"
+            )
+        endif()
     endif()
 endmacro()
 
 macro(silkit_install_debugsymbols targetName)
     if(MSVC)
-	message(STATUS "Installing .pdb symbols file")
-	install(
-		FILES $<TARGET_PDB_FILE:SilKit>
-		DESTINATION ${INSTALL_LIB_DIR}
-		COMPONENT bin
-	)
+        message(STATUS "Installing .pdb symbols file")
+        install(
+            FILES $<TARGET_PDB_FILE:SilKit>
+            DESTINATION ${INSTALL_LIB_DIR}
+            COMPONENT bin
+        )
     endif()
     if(APPLE)
         return()
@@ -50,14 +59,12 @@ macro(silkit_install_debugsymbols targetName)
 
         silkit_split_debugsymbols("${targetName}")
 
-        file(MAKE_DIRECTORY "${SILKIT_SYMBOLS_DIR}")
-
-	message(STATUS "Installing ELF debug file")
-	install(
-		FILES "${SILKIT_SYMBOLS_DIR}/${targetName}${CMAKE_DEBUG_POSTFIX}.debug"
-		DESTINATION ${INSTALL_LIB_DIR}
-		COMPONENT bin
-	)
+        message(STATUS "Installing ELF debug file")
+        install(
+            FILES "${SILKIT_SYMBOLS_DIR}/${targetName}${CMAKE_DEBUG_POSTFIX}.debug"
+            DESTINATION ${INSTALL_LIB_DIR}
+            COMPONENT bin
+        )
     endif()
 endmacro()
 
