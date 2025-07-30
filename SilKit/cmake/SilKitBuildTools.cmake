a08408f7fc39e6fe1c6440e6485fc4f40c0c1226
Marius Börschig <marius.boerschig@vector.com>
2025-07-30 16:14:16 +0200

    cmake: strip demos and utilities (#245)

--- SilKit/cmake/SilKitBuildTools.cmake
+++ SilKit/cmake/SilKitBuildTools.cmake
@@ -7,7 +7,7 @@ include(CMakeFindBinUtils)
 macro(silkit_split_debugsymbols targetName)
     set(_targetFile "$<TARGET_FILE:${targetName}>")
     set(_debugFile "${SILKIT_SYMBOLS_DIR}/${targetName}${CMAKE_DEBUG_POSTFIX}.debug")
-    message(STATUS "Split debugsymbols from ${_targetFile} into ${_debugFile}")
+    message(DEBUG "Split debugsymbols from ${_targetName} into ${_debugFile}")
     add_custom_command(
         TARGET ${targetName}
         POST_BUILD
@@ -16,6 +16,17 @@ macro(silkit_split_debugsymbols targetName)
         COMMAND ${CMAKE_OBJCOPY} --add-gnu-debuglink="${_debugFile}" "${_targetFile}"
         COMMENT "SIL Kit: SILKIT_PACKAGE_SYMBOLS: splitting ELF debug symbols"
     )
+endmacro()
+macro(silkit_strip_distrib targetName)
+    if(SILKIT_PACKAGE_SYMBOLS)
+        set(_targetFile "$<TARGET_FILE:${targetName}>")
+        add_custom_command(
+            TARGET ${targetName}
+            POST_BUILD
+            COMMAND ${CMAKE_STRIP} "${_targetFile}"
+            COMMENT "SIL Kit: stripping ${_targetName}"
+        )
+    endif()
 endmacro()
 
 macro(silkit_install_debugsymbols targetName)
