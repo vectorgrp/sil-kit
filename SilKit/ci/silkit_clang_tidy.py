a08bcb39367ab89474bffe9704550b225acc2977
Jan Kraemer <jan.kraemer@vector.com>
2025-09-10 14:27:52 +0200

    fixup! fixup! fixup! fixup! ci: add clang_tidy to the CI

--- SilKit/ci/silkit_clang_tidy.py
+++ SilKit/ci/silkit_clang_tidy.py
@@ -4,12 +4,14 @@
 #
 # SPDX-License-Identifier: MIT
 
+import argparse
 import json
 import os
 import subprocess
 import sys
 
 from multiprocessing import Pool, cpu_count
+from pathlib import Path
 
 isCI = os.getenv('CI')
 INFO_PREFIX = "::notice ::" if isCI is not None else "INFO: "
@@ -35,33 +37,59 @@ def die(status, fmt, *args):
     sys.exit(status)
 
 
-def run_clang_tidy(source_file: str):
-    clang_tidy = subprocess.run(['clang-tidy', source_file, '--header-filter', '\'^.*SilKit.*\'',
-                                 '-p', 'build_tidy/',
-                                 '--quiet'],
+def run_clang_tidy(source_file: Path, outdir: Path):
+
+    source_file_str = str(outdir / f"{source_file.name}_fixes.yaml")
+    cmd_args = ['clang-tidy', source_file, '--header-filter', '\'^.*SilKit.*\'',
+                '-p', 'build_tidy/',
+                '--export-fixes', source_file_str,
+                '--quiet']
+
+    clang_tidy = subprocess.run(cmd_args,
                                 capture_output=True, encoding='utf-8')
 
     if clang_tidy.stdout != "" and "ThirdParty" not in clang_tidy.stdout:
-        print(f"Processing source file {source_file}", flush=True)
-        print(f"{source_file} o:\n{clang_tidy.stdout}", flush=True)
+        print(f"{source_file}:\n{clang_tidy.stdout}", flush=True)
 
 
-with open('build_tidy/compile_commands.json', 'r') as f:
-    cc = json.load(f)
+def main():
 
-source_files = []
-for file_entry in cc:
-    # Exclude ThirdParty Stuff
-    filename = file_entry["file"]
-    if "ThirdParty" not in filename:
+    parser = argparse.ArgumentParser(
+            prog="ClangTidyRunner",
+            description="Run clang tidy on select source files")
 
-        # Skip F|I|UTests due to gmock macro hell
-        if "Test_" in filename:
-            continue
-        source_files.append(filename)
+    parser.add_argument(
+        'builddir', help="The build dir containing the compile_commands.json", type=Path)
+    parser.add_argument(
+        'outdir', help="The directory to store reports in", type=Path)
 
-print(f"Processing {len(source_files)} cpp files!")
+    args = parser.parse_args()
+    with open(args.builddir / 'compile_commands.json', 'r') as f:
+        cc = json.load(f)
 
-num_cpus = cpu_count()
-with Pool(num_cpus) as p:
-    p.map(run_clang_tidy, source_files)
+    source_files = []
+    for file_entry in cc:
+        # Exclude ThirdParty Stuff
+        filename = file_entry["file"]
+        if "ThirdParty" not in filename:
+
+            # Skip F|I|UTests due to gmock macro hell
+            if "Test_" in filename:
+                continue
+            source_files.append(filename)
+
+    print(f"Processing {len(source_files)} cpp files!")
+
+    global parallel_tidy
+
+    def parallel_tidy(source_file):
+        run_clang_tidy(Path(source_file), args.outdir)
+
+    num_cpus = cpu_count()
+
+    with Pool(num_cpus) as p:
+        p.map(parallel_tidy, source_files)
+
+
+if __name__ == "__main__":
+    main()
