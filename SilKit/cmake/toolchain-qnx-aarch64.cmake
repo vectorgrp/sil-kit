90bee539f91dc361f243643a71b1e60727727b32
Hasan.Grosic <hasan.grosic@vector.com>
2025-09-18 09:24:36 +0000

    Fix toolset for QNX aarch64

--- SilKit/cmake/toolchain-qnx-aarch64.cmake
+++ SilKit/cmake/toolchain-qnx-aarch64.cmake
@@ -3,6 +3,6 @@
 # SPDX-License-Identifier: MIT
 
 #NB: Call <<QNX_INSTALLATION_DIR>>/qnxsdp-env.sh before building
-set(SILKIT_TARGET_TOOLSET "gcc_ntoaarch64le_gpp")
+set(SILKIT_TARGET_TOOLSET "gcc_ntoaarch64le")
 set(SILKIT_TARGET_ARCHITECTURE "aarch64")
 include(${CMAKE_CURRENT_LIST_DIR}/qnx-cross-base-toolchain.cmake)
