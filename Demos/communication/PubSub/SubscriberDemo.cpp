86770779dcd32315767bf79ba3701fd244607ecd
Marius Börschig <Marius.Boerschig@vector.com>
2026-02-09 14:10:43 +0100

    revert demos
    Signed-off-by: Marius Börschig <Marius.Boerschig@vector.com>

--- Demos/communication/PubSub/SubscriberDemo.cpp
+++ Demos/communication/PubSub/SubscriberDemo.cpp
@@ -21,18 +21,25 @@ private:
 
     void CreateControllers() override
     {
-        auto subSpec = PubSubDemoCommon::dataSpecGps;
-        subSpec.AddLabel("Key1", "Value1", SilKit::Services::MatchingLabel::Kind::Optional);
-        subSpec.AddLabel("Key2", "Value2", SilKit::Services::MatchingLabel::Kind::Optional); // BUGHUNT: Missing second on publisher label breaks communication
-
         _gpsSubscriber = GetParticipant()->CreateDataSubscriber(
-            "GpsSubscriber", subSpec,
+            "GpsSubscriber", PubSubDemoCommon::dataSpecGps,
             [this](IDataSubscriber* /*subscriber*/, const DataMessageEvent& dataMessageEvent) {
             auto gpsData = PubSubDemoCommon::DeserializeGPSData(SilKit::Util::ToStdVector(dataMessageEvent.data));
 
             std::stringstream ss;
             ss << "Received GPS data: lat=" << gpsData.latitude << ", lon=" << gpsData.longitude
                << ", signalQuality=" << gpsData.signalQuality;
+            GetLogger()->Info(ss.str());
+        });
+
+        _temperatureSubscriber = GetParticipant()->CreateDataSubscriber(
+            "TemperatureSubscriber", PubSubDemoCommon::dataSpecTemperature,
+            [this](IDataSubscriber* /*subscriber*/, const DataMessageEvent& dataMessageEvent) {
+            double temperature =
+                PubSubDemoCommon::DeserializeTemperature(SilKit::Util::ToStdVector(dataMessageEvent.data));
+
+            std::stringstream ss;
+            ss << "Received temperature data: temperature=" << temperature;
             GetLogger()->Info(ss.str());
         });
     }
